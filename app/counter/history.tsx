1dc7c510ab7676df51aaeab22b5b3644322fda9e
utk09 <utk0997@gmail.com>
2024-10-23 11:57:19 +0100

    added history to show all the times the thing has been done

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -1,21 +1,70 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { countdownStorageKey, PersistedCountdownState } from ".";
+import { useEffect, useState } from "react";
+import { getFromStorage } from "../../utils/storage";
+import { format } from "date-fns";
+import { theme } from "../../theme";
+
+const fullDateFormat = `LLL d yyyy, h:mm aaa`;
 
 export default function HistoryScreen() {
+  const [countdownState, setCountdownState] =
+    useState<PersistedCountdownState>();
+
+  useEffect(() => {
+    const init = async () => {
+      const value = await getFromStorage(countdownStorageKey);
+      setCountdownState(value);
+    };
+    init();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History Screen</Text>
-    </View>
+    <FlatList
+      style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      data={countdownState?.completedAtTimestamps}
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text style={styles.emptyText}>No history</Text>
+        </View>
+      }
+      renderItem={({ item }) => (
+        <View style={styles.listItem}>
+          <Text style={styles.listItemText}>
+            {format(item, fullDateFormat)}
+          </Text>
+        </View>
+      )}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  list: {
     flex: 1,
-    backgroundColor: "#fffff",
+    backgroundColor: theme.colorWhite,
+  },
+  contentContainer: {
+    marginTop: 8,
+  },
+  listItem: {
+    backgroundColor: theme.colorGrey,
+    marginHorizontal: 8,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 8,
+  },
+  listItemText: {
+    fontSize: 18,
+  },
+  listEmptyContainer: {
     justifyContent: "center",
     alignItems: "center",
+    marginVertical: 24,
   },
-  text: {
+  emptyText: {
     fontSize: 24,
+    color: theme.colorDarkGrey,
   },
 });
